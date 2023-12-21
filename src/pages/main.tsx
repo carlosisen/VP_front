db4817ca0ee300a1e1dc94dc4ad584083f062fea
Carlos isen <amra_akyra@hotmail.com>
2023-12-21 12:35:53 +0000

    include advertisment

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,15 +1,22 @@
 import "@styles/main.css";
 import News from "@src/components/news";
 import VideoReviewSection from "@src/components/videoReviewSection";
+import advertisement from "@assets/images/banner_vp 1.png";
 const Main = () => {
   return (
     <div className="main">
-      <div className="main-div__section">
+      <div className="main__div__section">
         <News />
         PÁGINA EN DESARROLLO, ESPERAD CON UNAS BUENAS PALOMITAS
       </div>
-      <div className="main-div__section">
+      <div className="main__div__section">
         <VideoReviewSection />
+        <div className="main__div--advertisement">
+          <img
+            className="main__img"
+            alt="anuncio-camiseta 15€"
+            src={advertisement}></img>
+        </div>
       </div>
     </div>
   );
