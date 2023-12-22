859fe212ba0c4de83dd6617ba49a09a94d4cbc1e
Carlos isen <amra_akyra@hotmail.com>
2023-12-22 18:44:37 +0000

    styled: podcast

--- src/components/podcast.tsx
+++ src/components/podcast.tsx
@@ -1,3 +1,18 @@
 import Titles from "./general/titles";
-const Podcast = () => {};
+import text from "@src/constants/texts.json";
+import "@styles/podcast.css";
+const Podcast = () => {
+  const title = text[0].spanish.mainPage.podcast.title;
+  return (
+    <div className="podcast">
+      <Titles title={title} color="dark" size="little" icon="mphone" />
+      <iframe
+        style={{ width: "100%", height: "28rem" }}
+        className="main__iframe"
+        src="https://www.ivoox.com/player_ej_102722240_6_1.html?c1=5b4b98"
+        loading="lazy"
+        allowFullScreen={false}></iframe>
+    </div>
+  );
+};
 export default Podcast;
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,18 +2,13 @@ import "@styles/main.css";
 import News from "@src/components/news";
 import VideoReviewSection from "@src/components/videoReviewSection";
 import advertisement from "@assets/images/banner_vp 1.png";
+import Podcast from "@src/components/podcast";
 const Main = () => {
   return (
     <div className="main">
       <div className="main__div__section">
         <News />
-        <div className="main__div--advertisment">
-          <iframe
-            style={{ width: "100%" }}
-            className="main__iframe"
-            src="https://www.ivoox.com/player_ej_102722240_6_1.html?c1=5b4b98"
-            loading="lazy"></iframe>
-        </div>
+        <Podcast />
       </div>
       <div className="main__div__section">
         <VideoReviewSection />
